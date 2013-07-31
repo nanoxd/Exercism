b092c55527c5b858a1ba97aec362b0bd346e2711
Fernando Paredes <nano@fdp.io>
2013-07-30 17:19:12 -0700

    Pass Shouting and Asking Questions tests

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,5 +1,22 @@
 class Bob
+
   def hey(message)
-    "Whatever."
+    if has_question?(message)
+      "Sure."
+    elsif has_exclamation?(message)
+      "Woah, chill out!"
+    else
+      "Whatever."
+    end
   end
+
+  private
+
+    def has_question?(message)
+      message.scan(/\?/) == ["?"]
+    end
+
+    def has_exclamation?(message)
+      message.scan(/!/) == ["!"]
+    end
 end
